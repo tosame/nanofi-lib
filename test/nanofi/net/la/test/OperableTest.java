f43a1e392b6d0e05d1df5dd3cac637fef36da8e2
Kazuto Fukuchi <nanogenomu@gmail.com>
2012-10-06 05:48:55 +0900

    Clean files

--- test/nanofi/net/la/test/OperableTest.java
+++ test/nanofi/net/la/test/OperableTest.java
@@ -1,7 +1,9 @@
-package nanofi.net.la;
+package nanofi.net.la.test;
 
 import static org.junit.Assert.*;
 
+import nanofi.net.la.Operable;
+import nanofi.net.la.Vector;
 import org.junit.Test;
 import org.junit.experimental.runners.Enclosed;
 import org.junit.runner.RunWith;
